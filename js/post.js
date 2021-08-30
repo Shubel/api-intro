4fb7851a2cda390637b0b21986b42fbd1c9969dc
Sunny <shubelislam.diu@gmail.com>
2021-08-30 12:58:46 +0600

    dynamic tag and style added

--- js/post.js
+++ js/post.js
@@ -6,5 +6,16 @@ function posts() {
 posts();
 
 function displayPosts(posts) {
-    console.log(posts);
+    const divID = document.getElementById("divAdded");
+    // console.log(posts);
+    for (const post of posts) {
+        // console.log(post);
+        // console.log(post.title);
+        // console.log(post.body);
+        const divAdded = document.createElement("div");
+        divAdded.classList.add("postStyle");
+        divAdded.innerHTML = `<h2>Title : ${post.title}</h2>
+        <p>Body : ${post.body}</p>`
+        divID.appendChild(divAdded);
+    }
 };
